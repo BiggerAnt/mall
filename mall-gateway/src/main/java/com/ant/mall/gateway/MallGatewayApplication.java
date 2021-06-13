1da91dfa6bd0ff9a01b5b831aeb01176d9c7e94a
18340032515@163.com <18340032515@163.com>
2021-06-13 23:09:35 +0800

    1

--- mall-gateway/src/main/java/com/ant/mall/gateway/MallGatewayApplication.java
+++ mall-gateway/src/main/java/com/ant/mall/gateway/MallGatewayApplication.java
@@ -1,12 +1,15 @@
 package com.ant.mall.gateway;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 //开启服务注册与发现
 @EnableDiscoveryClient
+//@MapperScan("com.ant.mall.gateway.dao")
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})//排除和数据源相关的配置
 public class MallGatewayApplication {
 
